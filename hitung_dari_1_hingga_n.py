d4ee88be194d2a380c7e95cac79f40d4d843ee13
Alif-Co <alifhakimg@gmail.com>
2024-12-18 07:57:08 +0700

    perbaikan_alpro

---
+++ hitung_dari_1_hingga_n.py
@@ -0,0 +1,12 @@
+total = 0
+i = 1
+
+n = int(input("Masukkan nilai : "))
+
+while i <= n:
+    total += i
+    i += 1
+
+print("totalnya adalah : ", total)
+
+
---
+++ luas_lingkaran.py
@@ -0,0 +1,6 @@
+import math
+
+r = float(input("Masukkan jari jari lingkaran : "))
+calc = (22/7 * math.pow(r,2))
+
+print(f"Luas dari lingkaran dengan jari-jari {r} adalah : {calc}")
